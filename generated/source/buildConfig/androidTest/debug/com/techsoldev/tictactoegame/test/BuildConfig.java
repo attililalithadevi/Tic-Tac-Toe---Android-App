b864f10f9b0cc524b7a736bec3117465a97ae66c
Lalitha Devi Attili <134282048+attililalithadevi@users.noreply.github.com>
2024-07-01 20:03:55 +0530

    Add files via upload

---
+++ generated/source/buildConfig/androidTest/debug/com/techsoldev/tictactoegame/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.techsoldev.tictactoegame.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.techsoldev.tictactoegame.test";
+  public static final String BUILD_TYPE = "debug";
+}
